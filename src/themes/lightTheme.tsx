2fc15b3e7f300f1b9cb049bb2720586b97656e29
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-03-04 22:54:50 -0500

    style: change colors

--- src/themes/lightTheme.tsx
+++ src/themes/lightTheme.tsx
@@ -5,7 +5,7 @@ import { red } from '@mui/material/colors'
 export const lightTheme = createTheme({
     palette: {
         primary: {
-            main: '#556cd6'
+            main: '#19191D'
         },
         secondary: {
             main: '#19857b'
