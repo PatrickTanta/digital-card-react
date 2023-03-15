c1c3ace34bf67919b265d89646033cc848c339cd
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-03-15 17:54:33 -0500

    refactor: header page component

--- src/components/admin/HeaderPage.tsx
+++ src/components/admin/HeaderPage.tsx
@@ -5,9 +5,15 @@ interface Props {
     title: string
     btnTitle?: string
     btnTitleTwo?: string
+    onSelectFirstBtn: () => void
 }
 
-export const HeaderPage: FC<Props> = ({ title, btnTitle, btnTitleTwo }) => {
+export const HeaderPage: FC<Props> = ({
+    title,
+    btnTitle,
+    btnTitleTwo,
+    onSelectFirstBtn
+}) => {
     return (
         <Box
             sx={{}}
@@ -17,7 +23,15 @@ export const HeaderPage: FC<Props> = ({ title, btnTitle, btnTitleTwo }) => {
         >
             <h1>{title}</h1>
             <Box display="flex" sx={{ gap: 1 }}>
-                {btnTitle && <Button variant="contained">{btnTitle}</Button>}
+                {btnTitle && (
+                    <Button
+                        variant="contained"
+                        sx={{ textTransform: 'none' }}
+                        onClick={onSelectFirstBtn}
+                    >
+                        {btnTitle}
+                    </Button>
+                )}
                 {btnTitleTwo && (
                     <Button variant="contained">{btnTitleTwo}</Button>
                 )}
